fee08b012826af984b8017f53c2ebb540fbd816f
Lincoln Santana <62919518+lincolnsantana@users.noreply.github.com>
2025-11-10 17:03:41 -0300

    Aula 8.2: Como declarar ferramentas

--- mcp_server/index.ts
+++ mcp_server/index.ts
@@ -1,5 +1,6 @@
 import { McpServer } from "@modelcontextprotocol/sdk/server/mcp.js";
 import { StdioServerTransport } from "@modelcontextprotocol/sdk/server/stdio.js";
+import { z } from "zod";
 
 const server = new McpServer({
     name: "secretaria",
@@ -9,9 +10,25 @@ const server = new McpServer({
     }
 });
 
+server.tool(
+    "getTodayDate",
+    "Retorna a data de hoje",
+    {
+        locale: z.string().describe("Lugar onde você quer saber a data")
+    },
+    async ({ locale }) => {
+        const resultado = "01-05-2025";
+
+        return {
+            content: [{
+                type: "text",
+                text: resultado
+            }]
+        }
+    }
+);
+
 const transport = new StdioServerTransport();
-
 await server.connect(transport);
-
 console.error("MCP Server funcionando!");
     
